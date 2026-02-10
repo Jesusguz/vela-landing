ab4ae946160c5cc5f6637d914e9b9de1d22a92b4
JESUS MAGDIAL MENDEZ GUZMAN <119821074+Jesusguz@users.noreply.github.com>
2026-02-10 21:28:57 +0000

    fix: dynamic base path for cloudflare and github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://Jesusguz.github.io',
-  base: '/vela-landing',
+  //base: '/vela-landing',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/vela-landing' : '/',
   integrations: [tailwind()],
 });// Force build
